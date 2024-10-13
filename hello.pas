852c624df77a1aece1e6f9e936ed8bf564411797
bestdmit <bestdmit@gmail.com>
2024-10-13 21:28:35 +0300

    init

--- hello.pas
+++ hello.pas
@@ -1,2 +1,5 @@
 ﻿##
-print('Hello world');
+var a := readstring('Введите ваш язык');
+if a = 'русский' then println('привет мир');
+if a = 'английски' then print('hello world');
+if a = 'китайский' then print('你好世界');
